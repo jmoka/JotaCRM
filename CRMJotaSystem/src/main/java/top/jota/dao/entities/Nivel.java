cfe32d16a712cf064e81697da147cd81eb1ddc4d
jmoka <jmokatavares@gmail.com>
2023-12-03 12:38:21 -0300

    criação da entities nivel e estrutura dad.services com a classe serviceNivel

--- CRMJotaSystem/src/main/java/top/jota/dao/entities/Nivel.java
+++ CRMJotaSystem/src/main/java/top/jota/dao/entities/Nivel.java
@@ -3,5 +3,6 @@ package top.jota.dao.entities;
 import top.jota.dao.entities.abstracts.AbstractNivel;
 
 public class Nivel extends  AbstractNivel{
+	private static final long serialVersionUID = 1L;
 
 }
