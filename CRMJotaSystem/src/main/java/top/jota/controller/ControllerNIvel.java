c96508add72cadf992b9c0a8fae4db988263ef5b
jmoka <jmokatavares@gmail.com>
2023-12-05 23:31:36 -0300

    inserindo alert nas mensagens apos inserir nivel

--- CRMJotaSystem/src/main/java/top/jota/controller/ControllerNIvel.java
+++ CRMJotaSystem/src/main/java/top/jota/controller/ControllerNIvel.java
@@ -3,30 +3,45 @@ package top.jota.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+//import ch.qos.logback.core.model.Model;
 import top.jota.controller.inteface.InterfaceControllerDao;
 import top.jota.dao.entities.Nivel;
 import top.jota.dao.services.factorys.FactoryServices;
 
-@SpringBootApplication
-@RestController
+
+@Controller
 public class ControllerNIvel implements InterfaceControllerDao{
 	
 	@Autowired
 	@Qualifier("newNivel")
     private String newNivel;
 	
-	@GetMapping("/inserir")
-	public String inserir() {
-			String nome = newNivel ; 		
-			Nivel objNivel = new Nivel(nome);
-			FactoryServices.FactoryNivel().insert_Nivel(objNivel);			
-		return "Inserido com sucesso o" + " = " + objNivel ;
+	@PostMapping("/inserir")	
+	public String inserir(@RequestParam("nameNivel")String nomeNivel, Model model) {	// recebe do formulario o input			
+			Nivel objNivel = new Nivel(nomeNivel);
+			Nivel obj = FactoryServices.FactoryNivel().insert_Nivel(objNivel);	
+			
+			if(obj.getIdNivel() == null) {
+				model.addAttribute("mensagem", "Erro ao cadastrar nome ja existe = " + nomeNivel);
+				
+				
+			}else {
+				model.addAttribute("mensagem", "Nível cadastrado com sucesso: " + nomeNivel);
+				
+			}
+			
+				
+			 return "pageCadastroNivel";
+		
 	}
+	
+	
 
 	
 	
--- CRMJotaSystem/src/main/java/top/jota/controller/inteface/InterfaceControllerDao.java
+++ CRMJotaSystem/src/main/java/top/jota/controller/inteface/InterfaceControllerDao.java
@@ -1,7 +1,10 @@
 package top.jota.controller.inteface;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+
 public interface InterfaceControllerDao {
 	
-	public String inserir();
+	public String inserir(@RequestParam("nameNivel")String nomeNivel, Model model);
 	
 }
