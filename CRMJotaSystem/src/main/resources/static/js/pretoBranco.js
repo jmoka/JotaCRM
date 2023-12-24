7fc435cc8306d66d39fe487977a7fe2f62a2ce4e
jmoka <jmokatavares@gmail.com>
2023-12-23 22:49:07 -0300

    iniciando a implementação do checked

---
+++ CRMJotaSystem/src/main/resources/static/js/pretoBranco.js
@@ -0,0 +1,24 @@
+function pretoBranco() {
+            // Obtém o elemento do switch checkbox pelo ID
+            var pretoBranco = document.getElementById('pretoBranco');
+            var estiloPreto = document.getElementById('estiloPreto');
+            var estiloBranco = document.getElementById('estiloBranco');
+
+			
+            // Verifica se o switch checkbox está marcado ou não
+            var checked = pretoBranco.checked ? "Preto" : "Branco";
+            
+			if(checked == "Preto"){
+				estiloPreto.removeAttribute('disabled');
+                estiloBranco.setAttribute('disabled', 'true');
+                redirecionarPagina();
+				
+			}else
+				estiloBranco.removeAttribute('disabled');
+                estiloPreto.setAttribute('disabled', 'true');
+                redirecionarPagina();
+
+
+            // Exibe a informação em um alerta (você pode manipular a informação como desejar)
+            alert('O switch checkbox está ' + informacao);
+        }
