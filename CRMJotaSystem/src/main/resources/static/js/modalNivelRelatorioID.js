a51f32259a812664d19811a5070de0b4b6753770
jmoka <jmokatavares@gmail.com>
2023-12-27 17:11:13 -0300

    correção no datatable bootstrap 3

--- CRMJotaSystem/src/main/resources/static/js/modalNivelRelatorioID.js
+++ CRMJotaSystem/src/main/resources/static/js/modalNivelRelatorioID.js
@@ -1,13 +1,15 @@
-const myModal = document.getElementById('myModal');
+
+
+
+
+	
+	const myModal = document.getElementById('myModal');
 const idNivelSpan = document.getElementById('idNivelSpan');
 
 myModal.addEventListener('shown.bs.modal', () => {
     const idNivel = myModal.getAttribute('data-idNivel');
     idNivelSpan.textContent = idNivel;
-});
-
-
-
+	});
 
 
 
